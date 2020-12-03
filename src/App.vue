e9b1789842589fac53abb4d04a2d35dea73eb88f
Kevin <kevinuyttenhove@hotmail.com>
2020-12-03 10:54:25 +0100

    Added Fonts / Colors / Basic Layout

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/style/screen.scss";
+
+
+#nav {
+
+  a {
+    font-weight: bold;
+    color: white;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/poster.jpg" />
+    <div class="o-flex o-flex--space-between">
+      <h1>Movie Title</h1>
+      <div class="o-flex--space-between">
+        <h1 class="o-spacing--right">Size</h1>
+        <h1>Added</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
