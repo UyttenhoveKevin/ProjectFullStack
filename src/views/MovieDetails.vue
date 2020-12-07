f041cf02d4b5073267482336093c536843b8efa6
Kevin <kevinuyttenhove@hotmail.com>
2020-12-07 17:47:50 +0100

    Made MovieDetail Screen responsive for bigger screens

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    <div class="o-flex--space-between">
-      <h1> {{ $route.params.movieId + " (" + this.movie.dateReleased.slice(0,4) + ")"}} </h1>
-      <h1 class="u-font-color--grey"> {{movie.avarageScore}}/10</h1>
-    </div>
+    <div class="c-movieDetailContainer">
+      <div class="o-flex--space-between">
+        <h1> {{ $route.params.movieId + " (" + this.movie.dateReleased.slice(0,4) + ")"}} </h1>
+        <h1 class="u-font-color--grey"> {{movie.avarageScore}}/10</h1>
+      </div>
       <img :src="movie.default_image"/>
       <div class="o-flex--wrap">
         <tag v-for="tag in movie.tags" v-bind:key="tag.id" :tag="tag"/>
@@ -24,45 +25,47 @@
           No actors found
         </p>
       </div>
-    <div v-on:click="toggleReviews" class="o-flex--space-between u-hover">
-      <h2> Reviews ({{movie.reviews.length}}) </h2>
-      <div class="o-flex--row" v-if="!showReviews">
-        <p class="u-font-weight--light u-font-color--grey o-spacing--right-sm"> Show</p>
-        <svg xmlns="http://www.w3.org/2000/svg" width="18.286" height="10.286" viewBox="0 0 18.286 10.286">
-          <g id="Group_3" data-name="Group 3" transform="translate(-319 -48.748)">
-            <g id="_9851132801553239378" data-name="9851132801553239378" transform="translate(308.381 36)">
-              <path data-name="Path 3" d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
-              <path data-name="Path 13" d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+      <div v-on:click="toggleReviews" class="o-flex--space-between u-hover">
+        <h2> Reviews ({{movie.reviews.length}}) </h2>
+        <div class="o-flex--row" v-if="!showReviews">
+          <p class="u-font-weight--light u-font-color--grey o-spacing--right-sm"> Show</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="18.286" height="10.286" viewBox="0 0 18.286 10.286">
+            <g id="Group_3" data-name="Group 3" transform="translate(-319 -48.748)">
+              <g id="_9851132801553239378" data-name="9851132801553239378" transform="translate(308.381 36)">
+                <path data-name="Path 3" d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+                <path data-name="Path 13" d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+              </g>
+              <g id="_9851132801553239378-2" data-name="9851132801553239378" transform="translate(350.034 38.128) rotate(90)">
+                <path data-name="Path 3" d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+                <path  d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+              </g>
             </g>
-            <g id="_9851132801553239378-2" data-name="9851132801553239378" transform="translate(350.034 38.128) rotate(90)">
-              <path data-name="Path 3" d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
-              <path  d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+          </svg>
+        </div>
+        <div class="o-flex--row" v-else>
+          <p class="u-font-weight--light u-font-color--grey o-spacing--right-sm"> Hide</p>
+          <svg xmlns="http://www.w3.org/2000/svg" width="18.286" height="10.286" viewBox="0 0 18.286 10.286">
+            <g id="Group_4" data-name="Group 4" transform="translate(337.286 59.034) rotate(180)">
+              <g id="_9851132801553239378" data-name="9851132801553239378" transform="translate(308.381 36)">
+                <path  d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+                <path  d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+              </g>
+              <g id="_9851132801553239378-2" data-name="9851132801553239378" transform="translate(350.034 38.128) rotate(90)">
+                <path d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+                <path d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
+              </g>
             </g>
-          </g>
-        </svg>
-      </div>
-      <div class="o-flex--row" v-else>
-        <p class="u-font-weight--light u-font-color--grey o-spacing--right-sm"> Hide</p>
-        <svg xmlns="http://www.w3.org/2000/svg" width="18.286" height="10.286" viewBox="0 0 18.286 10.286">
-          <g id="Group_4" data-name="Group 4" transform="translate(337.286 59.034) rotate(180)">
-            <g id="_9851132801553239378" data-name="9851132801553239378" transform="translate(308.381 36)">
-              <path  d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
-              <path  d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
-            </g>
-            <g id="_9851132801553239378-2" data-name="9851132801553239378" transform="translate(350.034 38.128) rotate(90)">
-              <path d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
-              <path d="M8.072,14.591H20.218c.223,0,.4-.356.4-.8s-.181-.8-.4-.8H8.072c-.223,0-.4.356-.4.8S7.848,14.591,8.072,14.591Z" transform="translate(15.515 -1.866) rotate(45)" fill="#e6e6e6"/>
-            </g>
-          </g>
-        </svg>
-      </div>
+          </svg>
+        </div>
 
 
+      </div>
+      <review v-if="showReviews" v-for="review in movie.reviews" v-bind:key="review.id" :user-name="review.username" :review="review.review" :review-title="review.reviewTitle" :score="review.rating"/>
+      <div class="o-spacing--center">
+        <button class="c-button" v-on:click="addButtonClicked">Add to basket </button>
+      </div>
     </div>
-    <review v-if="showReviews" v-for="review in movie.reviews" v-bind:key="review.id" :user-name="review.username" :review="review.review" :review-title="review.reviewTitle" :score="review.rating"/>
-    <div class="o-spacing--center">
-      <button class="c-button" v-on:click="addButtonClicked">Add to basket </button>
-    </div>
+
   </div>
 </template>
 
@@ -101,5 +104,6 @@
 </script>
 <style lang="scss">
 @import "@/assets/style/6-components/primaryButton.scss";
+@import "@/assets/style/6-components/movieItem.scss";
 
 </style>
